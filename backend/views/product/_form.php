a2cb3cb594c3fd80238e930599cc851ea677b5af
Chibinha <2180623@my.ipleiria.pt>
2020-11-24 21:23:07 +0000

    AZ-79 #IN-PROGRESS Criar CRUD produtos

---
+++ backend/views/product/_form.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Product */
+/* @var $form yii\widgets\ActiveForm */
+
+$categories = ArrayHelper::map(\common\models\Category::find()->asArray()->all(), 'id', 'description');
+?>
+<div class="product-form">
+
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
+
+    <?= $form->field($model, 'product_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'unit_price')->textInput(['type' => 'number', 'step' => '0.01']) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'product_image')->fileInput()?>
+
+    <?= $form->field($model, 'id_category')->dropDownList($categories,['id','description']) ?>
+
+    <?= $form->field($model, 'is_discontinued')->checkbox() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ common/models/Product.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "product".
+ *
+ * @property int $id
+ * @property string $product_name
+ * @property float|null $unit_price
+ * @property bool $is_discontinued
+ * @property string $description
+ * @property string $product_image
+ * @property int $id_category
+ *
+ * @property Category $category
+ */
+class Product extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'product';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['product_name', 'unit_price' ,'description', 'product_image', 'id_category'], 'required'],
+            [['unit_price'], 'number'],
+            [['is_discontinued'], 'boolean'],
+            [['description'], 'string'],
+            [['id_category'], 'integer'],
+            [['product_name'], 'string', 'max' => 50],
+            [['product_image'], 'file', 'extensions' => 'jpeg,jpg,png'],
+            [['id_category'], 'exist', 'skipOnError' => true, 'targetClass' => Category::className(), 'targetAttribute' => ['id_category' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'product_name' => 'Product Name',
+            'unit_price' => 'Unit Price',
+            'is_discontinued' => 'Is Discontinued?',
+            'description' => 'Description',
+            'product_image' => 'Product Image',
+            'id_category' => 'Category',
+        ];
+    }
+
+    /**
+     * Gets query for [[Category]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Category::className(), ['id' => 'id_category']);
+    }
+}
