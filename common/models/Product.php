41bf4bdd118611535c1e60b14116806186498f7b
Chibinha <2180623@my.ipleiria.pt>
2020-11-28 20:00:37 +0000

    AZ-79 #DONE Fixing not being able to update without uploading image

--- common/models/Product.php
+++ common/models/Product.php
@@ -33,7 +33,7 @@ class Product extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['product_name', 'unit_price' ,'description', 'product_image', 'id_category'], 'required'],
+            [['product_name', 'unit_price' ,'description', 'id_category'], 'required'],
             [['unit_price'], 'number'],
             [['is_discontinued'], 'boolean'],
             [['description'], 'string'],
