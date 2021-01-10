c4d1a3746a351e068c075ade1278523936ae3e28
Chibinha <2180623@my.ipleiria.pt>
2021-01-10 01:46:49 +0000

    Category Unit and Integration Tests

---
+++ common/tests/unit/models/CategoryTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace common\tests\unit;
+
+use Codeception\Test\Unit;
+use common\models\Category;
+
+class CategoryTest extends Unit
+{
+    /**
+     * @var \frontend\tests\UnitTester
+     */
+    protected $tester;
+    protected $category_id;
+
+    public function _before()
+    {
+        $this->category_id = $this->tester->haveRecord('common\models\Category', [
+            'description' => '123',
+            'parent_id' => '',
+        ]);
+    }
+
+    public function testDescriptionNull()
+    {
+        $category = new Category();
+        $category->description = null;
+
+        expect($category->validate())->false();
+        expect($category->hasErrors())->true();
+    }
+
+    public function testDescriptionCorrect()
+    {
+        $category = new Category();
+        $category->description = 'Teclados';
+
+        expect($category->validate())->true();
+    }
+
+    public function testParentIdIncorrect()
+    {
+        $category1 = new Category();
+        $category1->parent_id = null;
+        $category2 = new Category();
+        $category2->parent_id = 'abc';
+
+        expect($category1->validate())->false();
+        expect($category2->validate())->false();
+        expect($category1->hasErrors())->true();
+        expect($category2->hasErrors())->true();
+    }
+
+    function testCreatingCategory()
+    {
+        $cat = new Category();
+        $cat->description = "description";
+        $cat->save();
+        $this->tester->seeRecord('common\models\Category', ['description' => 'description']);
+    }
+
+    function testUpdatingCategory(){
+        $this->tester->seeRecord('common\models\Category', ['description' => '123']);
+        $category = Category::find($this->category_id)->One();
+        $category->description = "changedDesc";
+        $category->save();
+        $this->tester->seeRecord('common\models\Category', ['description' => 'changedDesc']);
+    }
+
+    function testDeletingCategory(){
+        $this->tester->seeRecord('common\models\Category', ['description' => '123']);
+        $category = Category::find()->where(['description' => '123'])->One();
+        $category->delete();
+        $this->tester->dontSeeRecord('common\models\Category', ['description' => '123']);
+    }
+}
+
+
+    
+
+
+  
